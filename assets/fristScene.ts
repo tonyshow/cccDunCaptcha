7ec9de6d26611b7f47a501a57b6b9df0acbe3d51
show <show@outllook.com>
2020-03-30 08:41:20 +0800

    add

--- assets/fristScene.ts
+++ assets/fristScene.ts
@@ -1,17 +1,14 @@
-// Learn TypeScript:
-//  - https://docs.cocos.com/creator/manual/en/scripting/typescript.html
-// Learn Attribute:
-//  - https://docs.cocos.com/creator/manual/en/scripting/reference/attributes.html
-// Learn life-cycle callbacks:
-//  - https://docs.cocos.com/creator/manual/en/scripting/life-cycle-callbacks.html
-
 const { ccclass, property } = cc._decorator;
 
 @ccclass
 export default class fristScene extends cc.Component {
   private AppClassName: string = "org/cocos2dx/javascript/AppActivity";
   eve() {
-    jsb.reflection.callStaticMethod(this.AppClassName, "openCaptcha", "()V");
+    // jsb.reflection.callStaticMethod(this.AppClassName, "openCaptcha", "()V");
+    let obj = {
+      funName: "openCaptcha"
+    }
+    console.error(`ts , fristScene:${ JSON.stringify(obj)}`);
+    jsb.reflection.callStaticMethod(this.AppClassName, "jsToJavaFunc", "(Ljava/lang/String;)V", JSON.stringify(obj));
   }
-  // update (dt) {}
 }
