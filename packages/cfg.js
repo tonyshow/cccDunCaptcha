09a289a62f491fe6a46f1ce3d6044e8a6103f304
ryyl <aa@qq.com>
2020-03-31 17:50:08 +0800

    add

---
+++ packages/cfg.js
@@ -0,0 +1,44 @@
+var app = module.exports = {};
+
+app.getCfgAll = function()
+{
+    let list = [
+        //packagesGroup---------------------------------------------------------------------------------------------------------------------------------------- 
+        {
+            updateLevel: 2,
+            describe: "引擎自定义修改",
+            modulePath: "./../../",
+            path: 'packages/cccEngineDefault',
+            url: "https://github.com/tonyshow/cccEngineDefault.git"
+        },
+        {
+            updateLevel: 2,
+            describe: "工具",
+            modulePath: "./../../",
+            path: 'packages/tools',
+            url: "https://github.com/tonyshow/tools.git"
+        },
+        {
+            updateLevel: 2,
+            describe: "打包",
+            modulePath: "./../../",
+            path: 'packages/pack',
+            url: "https://github.com/tonyshow/pack.git"
+        },
+        {
+            updateLevel: 2,
+            describe: "通用模块",
+            modulePath: "./../../",
+            path: 'assets/common',
+            url: "https://github.com/tonyshow/cccCommonSrc.git"
+        },
+        {
+            updateLevel: 2,
+            describe: "通用模块资源",
+            modulePath: "./../../",
+            path: 'assets/resources/common',
+            url: "https://github.com/tonyshow/cccResCommon.git"
+        }
+    ];
+    return list;
+}
