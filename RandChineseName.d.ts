dfa80cbcffec0b45d852e9553731440e97bd9aff
ryyl <aa@qq.com>
2020-03-31 17:44:56 +0800

    add

---
+++ RandChineseName.d.ts
@@ -0,0 +1,12 @@
+// Type definitions for crypto-js v3.1.8
+// Project: https://github.com/evanvosberg/crypto-js
+// Definitions by: Michael Zabka <https://github.com/misak113>
+// Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
+
+declare var chineseRandomName: ChineseRandomName;
+declare class ChineseRandomName {
+ 
+	generate(): string{
+		 return ""
+	}
+}
